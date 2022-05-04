69edc9fc094a61947d75060070ee7da0a504a61d
NarimanAlmanov <96466452+NarimanAlmanov@users.noreply.github.com>
2022-05-04 11:57:16 +0600

    Info

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        showInfo();
+    }
+    public static void showInfo(){
+        System.out.println("#==================================================#");
+        System.out.println("# Hash table and binary search tree implementation #");
+        System.out.println("# Made by Nariman Almanov                          #");
+        System.out.println("# 2022                                             #");
+        System.out.println("#==================================================#");
     }
 }

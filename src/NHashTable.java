defc12386ed2dd51d10cb7685dabf502fd625c76
Нариман <xx7namiran@gmail.com>
2022-05-08 21:29:08 +0600

    .

---
+++ src/NHashTable.java
@@ -0,0 +1,49 @@
+public class NHashTable <K, V>{
+    private class HashNode<K, V> {
+        private K key;
+        private V value;
+        private HashNode<K, V> next;
+
+        public HashNode(K key, V value)
+        {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "{" + key + " " + value + "}";
+        }
+    }
+
+    private HashNode<K, V>[] chainArray;
+    private int M = 11; // default number of chains
+    private int size;
+
+    public NHashTable() {}
+
+    public NHashTable(int M) {}
+
+    private int hash(K key) {
+        return key.hashCode();
+    }
+
+    public void put(K key, V value) {}
+
+    public V get(K key) {
+        return null;
+    }
+
+    public V remove(K key) {
+        return null;
+    }
+
+    public boolean contains(V value) {
+        return false;
+    }
+
+    public K getKey(V value) {
+        return null;
+    }
+}
